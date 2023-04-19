d2489a80e974a97dc6ee554796efbd0c78405864
Anna Lorencová <a.lorencov@seznam.cz>
2023-04-19 22:17:17 +0200

    audio download added

--- Downloader_audio.py
+++ Downloader_audio.py
@@ -1,10 +1,14 @@
 from pytube import YouTube
-from sys import argv
-
+import os
 link = input("Enter link:\t")
 yt = YouTube(link)
 
 print("Title\t", yt.title)
 print("View\t", yt.views)
-yd = yt.streams.get_audio_only()
-yd.download("./audios")
+yd = yt.streams.filter(only_audio=True).first()
+out_file = yd.download("./audios")
+
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
+print('Got it :)')
