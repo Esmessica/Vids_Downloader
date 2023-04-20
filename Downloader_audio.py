aa6fbe08552a281756d1f2fd731d73d09d39460c
Anna Lorencová <a.lorencov@seznam.cz>
2023-04-20 20:51:04 +0200

    while loop added for downloading multiple links

--- Downloader_audio.py
+++ Downloader_audio.py
@@ -1,14 +1,24 @@
 from pytube import YouTube
 import os
-link = input("Enter link:\t")
-yt = YouTube(link)
+on = True
+while on:
+    link = input("Enter link:\t")
+    yt = YouTube(link)
 
-print("Title\t", yt.title)
-print("View\t", yt.views)
-yd = yt.streams.filter(only_audio=True).first()
-out_file = yd.download("./audios")
+    print("Title\t", yt.title)
+    print("View\t", yt.views)
+    yd = yt.streams.filter(only_audio=True).first()
+    out_file = yd.download("./audios")
 
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp3'
-os.rename(out_file, new_file)
-print('Got it :)')
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    print('Got it :)')
+    ask = input("Do you want another audio? y/n\t")
+    if ask == "n":
+        on = False
+    elif ask == "y":
+        on = True
+    else:
+        raise Exception("Wrong commmand")
+        on = True
